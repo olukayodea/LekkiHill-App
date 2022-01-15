9186749c343adc1da733fb0630af02d147784950
Olukayode <olukayode.adebiyi@hotmail.co.uk>
2022-01-14 21:24:51 -0500

    Capacitor Init
    Added mobile app capabilities

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lekkihill.clinic',
+  appName: 'LekkiHill',
+  webDir: 'dist/LekkiHill',
+  bundledWebRuntime: false
+};
+
+export default config;
